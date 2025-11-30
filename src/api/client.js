55f50381e89315961a36a79bbdc43f28f9c3cccc
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-11-30 14:34:26 +0530

    Pakka final wala final 2.0

--- src/api/client.js
+++ src/api/client.js
@@ -6,7 +6,7 @@ import { Platform } from 'react-native';
 // e.g., 'http://192.168.1.10:5001'
 // For Android Emulator, use 'http://10.0.2.2:5001'
 // For iOS Simulator, use 'http://localhost:5001'
-const BASE_URL = 'http://192.168.143.216:5001';
+const BASE_URL = 'https://apk-project.onrender.com';
 
 const client = axios.create({
     baseURL: BASE_URL,
