ebeb9184ee0a9b217a72a51fce7ad10232a566cf
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-11-30 14:40:26 +0530

    Increase body size limit

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -41,7 +41,7 @@ const ProfileScreen = ({ navigation }) => {
         }
 
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            mediaTypes: ImagePicker.MediaType.Images,
             allowsEditing: true,
             aspect: [1, 1],
             quality: 0.5,
