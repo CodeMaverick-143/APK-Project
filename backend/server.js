a077f19909eb52f92ee2b94b1da1c2e4adb1639e
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-12-19 15:44:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-// Routes
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/tasks', require('./routes/taskRoutes'));
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
