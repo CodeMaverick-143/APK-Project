817008483b4764fe08d5814f4e82eecd0051cc9a
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2025-12-29 10:11:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/tasks', require('./routes/taskRoutes'));
 
+app.head('/', (req, res) => {
+    res.status(200).end();
+});
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
